7c0d372c16ca40f47d132ec2dca1b6b84b03f1d2
Johannes Loewe <johannes@loewe.pm>
2017-07-28 17:22:05 +0200

    added App class

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,57 @@
+import * as bodyParser from "body-parser";
+import * as config from "config";
+import { EventEmitter } from "events";
 import * as express from "express";
-import {Application} from "express";
-import bodyParser = require("body-parser");
-const app: Application = express();
+import * as mongoose from "mongoose";
+import * as morgan from "morgan";
+import * as path from "path";
+import { getMessage, postMessage } from "./controllers/message.controller";
 
-app.use(bodyParser.json({type: "application/json"}));
+export class App extends EventEmitter {
 
-const host = process.env.YOUR_HOST || "0.0.0.0";
-const port = process.env.PORT || 8080;
-app.listen(port, host, () => {
-    console.info(`App listening on port ${port}!`);
-});
+    public express: express.Application;
+
+    constructor() {
+        super();
+        this.express = express();
+        this.middleware();
+        this.routes();
+    }
+
+    public async run(port: number, dbURI: string): Promise<void> {
+        try {
+            await mongoose.connect(dbURI, { useMongoClient: true });
+            if (process.env.NODE_ENV !== "test") {
+              console.log(`Mongoose connected to ${dbURI}`);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        this.express.listen(port, () => {
+            if (process.env.NODE_ENV !== "test") {
+                console.log(`App listening on port ${port}`);
+            }
+            this.emit("ready");
+        });
+    }
+
+    private middleware(): void {
+        // Logger
+        if (process.env.NODE_ENV !== "test") {
+            this.express.use(morgan("combined"));
+        }
+        // Body parser
+        this.express.use(bodyParser.json());
+        // Query string parser
+        this.express.use(bodyParser.urlencoded({
+            extended: true,
+        }));
+    }
+
+    private routes(): void {
+        const router = express.Router();
+        router.get("/messages/:id", getMessage);
+        router.post("/messages", postMessage);
+        this.express.use("/", router);
+    }
+}
